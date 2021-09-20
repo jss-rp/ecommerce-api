2dcbefe821ccfc1f2e38c3e772759f2dc3a4f2d7
Jonatan Silva <jonatan.silva4@fatec.sp.gov.br>
2021-09-20 19:50:47 -0300

    Initial commit

---
+++ src/test/java/jss/ecommerce/api/EcommerceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package jss.ecommerce.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
