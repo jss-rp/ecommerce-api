747f5baf7c53968f9a1ef91f21b72b8a7d0c700a
Jonatan Silva <jonatan.silva4@fatec.sp.gov.br>
2021-09-28 20:43:12 -0300

    Ref: Config's optimization

--- src/main/java/jss/ecommerce/api/config/JerseyConfig.java
+++ src/main/java/jss/ecommerce/api/config/JerseyConfig.java
@@ -1,9 +1,5 @@
 package jss.ecommerce.api.config;
 
-import jss.ecommerce.api.resource.FabricaResource;
-import jss.ecommerce.api.resource.FabricanteResource;
-import jss.ecommerce.api.resource.LoteResource;
-import jss.ecommerce.api.resource.ProdutoResource;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,9 +7,6 @@ import org.springframework.context.annotation.Configuration;
 public class JerseyConfig extends ResourceConfig {
 
     public JerseyConfig() {
-        register(ProdutoResource.class);
-        register(FabricanteResource.class);
-        register(LoteResource.class);
-        register(FabricaResource.class);
+        register("jss.ecommerce.api.resources");
     }
 }
